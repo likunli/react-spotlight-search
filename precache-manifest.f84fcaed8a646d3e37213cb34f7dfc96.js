91c04d935532ae840b5705293242e62142730be1
Likun Li <likunli@Likuns-MacBook-Pro.local>
2020-09-26 13:55:29 -0700

    Updates

---
+++ precache-manifest.f84fcaed8a646d3e37213cb34f7dfc96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48aabd9e0e2551c2bc293b5d06a05479",
+    "url": "/react-spotlight-search/index.html"
+  },
+  {
+    "revision": "620fd044ea5bb04a8627",
+    "url": "/react-spotlight-search/static/css/main.9c7ee562.chunk.css"
+  },
+  {
+    "revision": "c0481195b746d07404cf",
+    "url": "/react-spotlight-search/static/js/2.3ba83300.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-spotlight-search/static/js/2.3ba83300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620fd044ea5bb04a8627",
+    "url": "/react-spotlight-search/static/js/main.7d230496.chunk.js"
+  },
+  {
+    "revision": "202978edaed581d51674",
+    "url": "/react-spotlight-search/static/js/runtime-main.cd3f3fa6.js"
+  }
+]);
